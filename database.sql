f4dd08404130718a8632d25cddd3dbdc67a8218c
AuraSalomaa <aura@salomaa.info>
2024-04-08 09:02:16 +0300

    lisätty etunimi ja sukunimi

--- database.sql
+++ database.sql
@@ -7,6 +7,8 @@ USE ChillCheck;
 CREATE TABLE Users (
     user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
+    Firstname TEXT(30) NOT NULL,
+    Lastnamne TEXT(30) NOT NULL,
     password  VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
