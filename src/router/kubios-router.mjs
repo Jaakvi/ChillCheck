7dd55dbbc5cde5b8e49b644cda6798a9a0eb822c
AuraSalomaa <aura@salomaa.info>
2024-05-06 13:26:02 +0300

    updating documentation

--- src/router/kubios-router.mjs
+++ src/router/kubios-router.mjs
@@ -3,8 +3,25 @@ import {authenticateToken} from '../middlewares/authentication.mjs';
 import {getUserData, getUserInfo} from '../controllers/kubios-controller.mjs';
 
 const kubiosRouter = express.Router();
+// /api/kubios/user-data endpoint
+  /**
+   * @api {get} /user-data Requests the users information
+   * @apiName KubiosInformation
+   * @apiGroup Kubios
+   * @apiPermission token
+   *
+   * @apiSuccess {Array} user[] array of User results
+   * @apiSuccess {Object} result.
+   * @apiSuccess {String} user.username Username
+   * @apiSuccess {String} user.user_level Userlevel of the User.
+   *
+   * TODO: add example response
+   *
+   * @apiUse InvalidTokenError
+   */
 
 kubiosRouter
+
   .get('/user-data', authenticateToken, getUserData)
   .get('/user-info', authenticateToken, getUserInfo)
 
