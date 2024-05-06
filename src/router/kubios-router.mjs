b9adbc44d039cc59fc1db725db7730de833c420d
AuraSalomaa <aura@salomaa.info>
2024-05-06 14:22:22 +0300

    pushing

--- src/router/kubios-router.mjs
+++ src/router/kubios-router.mjs
@@ -8,12 +8,31 @@ const kubiosRouter = express.Router();
    * @apiGroup Kubios
    * @api {get} /user-data Requests the users information
    * @apiPermission token
-   * @apiSuccess {Array} Results[] array of User results
-   * @apiSuccess {Object} result.
-   * @apiSuccess {String} user.username Username
-   * @apiSuccess {String} user.user_level Userlevel of the User.
-   *
-   * TODO: add example response
+   * @apiSuccessExample {json} Success-Response: {
+    "results": [
+        {
+            "create_timestamp": "2024-02-09T08:40:57.445402+00:00",
+            "daily_result": null,
+            "measure_id": "f4e3319d-df85-40e5-80fc-15b31f10e31a",
+            "measured_timestamp": "2024-02-09T10:37:37+02:00",
+            "result": {
+                "artefact": 46.263032720633134,
+                "artefact_level": "VERY LOW",
+                "freq_domain": {
+                    "HF_peak": 0.15,
+                    "HF_power": 129.49981720735522,
+                    "HF_power_nu": 38.31892464924658,
+                    "HF_power_prc": 33.55465770284691,
+                    "LF_HF_power": 1.6079062511664375,
+                    "LF_peak": 0.04666666666666667,
+                    "LF_power": 208.22356561261745,
+                    "LF_power_nu": 61.613238481499266,
+                    "LF_power_prc": 53.9527438761576,
+                    "VLF_peak": 0.04,
+                    "VLF_power": 47.98429485325251,
+                    "VLF_power_prc": 12.43319584254917,
+                    "tot_power": 385.9369341632949
+                }, {Array} Results[] array of User results
    *
    */
 
