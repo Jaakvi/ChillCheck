214816bb9f2fd144b323df88f1c6f78572b8afc3
AuraSalomaa <aura@salomaa.info>
2024-05-11 13:21:03 +0300

    added docs

--- src/router/kubios-router.mjs
+++ src/router/kubios-router.mjs
@@ -3,6 +3,87 @@ import {authenticateToken} from '../middlewares/authentication.mjs';
 import {getUserData, getUserInfo} from '../controllers/kubios-controller.mjs';
 
 const kubiosRouter = express.Router();
+// /api/kubios/user-data endpoint
+  /**
+   * @api {get} /user-data Requests the users information
+   * @apiname getUserdata
+   * @apiGroup Kubios
+   * @apiPermission token
+   * @apiSuccessExample {json} Success-Response
+   * {
+    results": [
+        {
+            "create_timestamp": "2024-02-09T08:40:57.445402+00:00",
+            "daily_result": null,
+            "measure_id": "f4e3319d-df85-40e5-80fc-15b31f10e31a",
+            "measured_timestamp": "2024-02-09T10:37:37+02:00",
+            "result": {
+                "artefact": 46.263032720633134,
+                "artefact_level": "VERY LOW",
+                "freq_domain": {
+                    "HF_peak": 0.15,
+                    "HF_power": 129.49981720735522,
+                    "HF_power_nu": 38.31892464924658,
+                    "HF_power_prc": 33.55465770284691,
+                    "LF_HF_power": 1.6079062511664375,
+                    "LF_peak": 0.04666666666666667,
+                    "LF_power": 208.22356561261745,
+                    "LF_power_nu": 61.613238481499266,
+                    "LF_power_prc": 53.9527438761576,
+                    "VLF_peak": 0.04,
+                    "VLF_power": 47.98429485325251,
+                    "VLF_power_prc": 12.43319584254917,
+                    "tot_power": 385.9369341632949
+                },
+                "mean_hr_bpm": 61.59545371800872,
+                "mean_rr_ms": 974.0978656426025,
+                "pns_index": -0.18524754700382892,
+                "readiness": 62.5,
+                "recovery": 62.5,
+                "respiratory_rate": 7.16,
+                "rmssd_ms": 25.42644515775738,
+                "sd1_ms": 18.046375575058658,
+                "sd2_ms": 32.79494825201846,
+                "sdnn_ms": 26.425617711159493,
+                "sns_index": 0.5293473600094027,
+                "stress_index": 14.660666125666104
+            },
+            "result_id": "213136ba-050a-4b9f-8b4d-24ac2d4cfd19",
+            "result_type": "readiness"
+        },
+  * @apiSuccess [array] results that include results of the measurment
+  * @apiErrorExample {json} Error-response unauthorized token invalid or old 403:
+    *{
+    "message": "invalid token"
+      }
+
+  */
+  /**
+  * @api {get} /user-info Requests the users account information
+  * @apiname getUserprofile
+  * @apiGroup Kubios
+  * @apiPermission token
+  * @apiSuccessExample {json} Success-Response
+   * {
+    {
+    "status": "ok",
+    "user": {
+        "email": "john.doe@email.com",
+        "family_name": "Doe",
+        "given_name": "John",
+        "sub": "0ac6f509-3411-4d41-94b7-90d863ikf42bd"
+    }
+}
+  *
+  *
+  * @apiErrorExample {json} Error-Response invalid token 403:
+  * {
+    "message": "invalid token"
+    }
+
+
+  */
+
 
 kubiosRouter
   .get('/user-data', authenticateToken, getUserData)
--- src/router/user.router.mjs
+++ src/router/user.router.mjs
@@ -14,24 +14,24 @@ import { validationErrorHandler } from '../middlewares/error-handler.mjs';
 
 const userRouter = express.Router();
 
-/**
- * @api {get} /users Request user list
- * @apiName GetUsers
- * @apiGroup Users
- * @apiPermission token
- *
- * @apiSuccess {Array} user[] array of Users.
- * @apiSuccess {object} user User object.
- * @apiSuccess {number} user.user_id Id of the User.
- * @apiSuccess {String} user.username Username.
- *
- *
- */
 // /user endpoint
 userRouter.route('/')
   // list users
   .get(authenticateToken, getUsers)
-    // update user
+  /**
+   * @api {get} /users Request user list
+   * @apiName GetUsers
+   * @apiGroup Users
+   * @apiPermission token
+   *
+   * @apiSuccess {Array} user[] array of Users.
+   * @apiSuccess {Object} user User object.
+   * @apiSuccess {Number} user.user_id Id of the user.
+   * @apiSuccess {String} user.username Username
+   * @apiSuccess {String} user.user_level Userlevel of the User.
+   *
+   */
+    // User registeration
   .post(
     body('username').trim().isLength({min: 3, max:30}).isAscii(),
     body('Firstname').trim().isLength({min:2, max: 20}).isAscii(),
@@ -39,17 +39,45 @@ userRouter.route('/')
     body('password').trim().isLength({min:8, max: 128}),
     body('email').trim().isEmail().normalizeEmail(),
     postUser);
+    /**
+   * @api {post} /users Register user
+   * @apiName Post Users
+   * @apiGroup Users
+   * will be implemented to work with kubios
+   */
 
 // /user/:id endpoint
 userRouter.route('/:id')
   // update user
+   /**
+   * @api {PUT} /users/:id information update
+   * @apiName UpdateUsers
+   * @apiGroup Users
+   * @apiPermission token
+   * @apiParam {number} users id
+   */
+  //  will be implemented to work with kubios
   .put(authenticateToken,putUser)
-  // get info of a user
+
+  // get user by id
+  /**
+   * @api {Get} /users/:id get user by their id
+   * @apiName GetUsersByID
+   * @apiGroup Users
+   * @apiPermission token
+   * @apiParam {number} users id
+   */
+   //  will be implemented to work with kubios
   .get(authenticateToken,validationErrorHandler,getUserById)
 
   // delete user based on id
+    /**
+   * @api {DELETE} /users/:id Delete user by id
+   * @apiName DeleteUserByID
+   * @apiGroup Users
+   * @apiPermission token
+   * @apiParam {number} users id
+   */
+   // will be implemented to work with kubios
   .delete(authenticateToken,deleteUser);
-
-// user login
-
 export default userRouter;
